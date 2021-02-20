51a96b8ba7e1501e159d84e5343b12c4cae3f5dd
Diane Compaore <dianeyasminec60@gmail.com>
2021-02-20 22:56:40 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,7 @@
-# code your #valid_move? method here
-
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def some_new_fabulous_method
+    if position_taken?
+        execute something
+    else
+        execute something else
+    end
+some_new_fabulous_method
