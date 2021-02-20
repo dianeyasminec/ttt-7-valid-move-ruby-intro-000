150bf17600cad789813c9dfcb92bc09ea2977eaa
Diane Compaore <dianeyasminec60@gmail.com>
2021-02-20 23:22:01 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,7 +10,7 @@ def position_taken? (board=[],index=0)
 end
 
  def valid_move? (board=[],index=0)
-    if index.between?(0,8) && position_taken?(board,index)
+    if index.between?(0,8) && !position_taken?(board,index)
       return true
     else
       return false
